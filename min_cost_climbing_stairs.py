2c1d81373a21991b9dc57953dde7a739003048e4
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-08-30 18:39:56 -0400

    min cost climbing stairs

---
+++ min_cost_climbing_stairs.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+
+        def rec(i):
+            if i >= n:
+                return 0
+            return cost[i] + min(rec(i+1), rec(i+2))
+        return min(rec(0), rec(1))
