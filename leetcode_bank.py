c11d20932ba1955d4da539a464c9cbd3e529faec
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-10-25 11:31:22 -0400

    leetcode bank

---
+++ leetcode_bank.py
@@ -0,0 +1,8 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        res = 0
+        for i in range(n):
+            addition = i//7 + 1
+            dayAddition = i % 7
+            res += addition + dayAddition
+        return res
