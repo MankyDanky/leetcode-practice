9ce57c1bf3e3dc08d25ed00083e12517fe7c5ad5
Aadi Kulshrestha <aadi.kulsh@gmail.com>
2025-12-25 00:01:32 -0500

    child happiness

---
+++ maximize_child_happiness.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
+        n = len(happiness)
+        happiness.sort(reverse=True)
+        res = 0
+        for i in range(k):
+            res += max(0, happiness[i] - i)
+        return res
