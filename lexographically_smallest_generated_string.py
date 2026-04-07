ef3e4ed28ae7722e41f89cdb3b44029f93a6a5dd
Aadi Kulshrestha <aadi.kulsh@shopify.com>
2026-04-07 09:51:54 -0400

    walking robot sim

--- lexographically_smallest_generated_string.py
+++ lexographically_smallest_generated_string.py
@@ -3,8 +3,7 @@ class Solution:
         n, m = len(str1), len(str2)
         s = ["a"] * (n + m - 1)
         fixed = [False] * (n + m - 1)
-
-        # process the case of 'T'
+        
         for i, ch in enumerate(str1):
             if ch == "T":
                 for j, c in enumerate(str2, i):
@@ -12,14 +11,11 @@ class Solution:
                         return ""
                     s[j], fixed[j] = c, True
 
-        # process the case of 'F'
         for i, ch in enumerate(str1):
             if ch == "F":
-                # check if there are already different characters
                 if any(str2[j - i] != s[j] for j in range(i, i + m)):
                     continue
 
-                # find the first modifiable position
                 for j in range(i + m - 1, i - 1, -1):
                     if not fixed[j]:
                         s[j] = "b"
