c6464f4924a845947a045f13aefeed488323825b
Aadi Kulshrestha <62843416+MankyDanky@users.noreply.github.com>
2026-06-01 09:19:34 -0700

    Create minimum_cost_buying_candies.cpp

---
+++ minimum_cost_buying_candies.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end());
+        int n = cost.size();
+        int res = 0;
+        for (int i = n-1; i >=0; i-=3) {
+            res += cost[i];
+            if (i > 0) res += cost[i-1];
+        }
+        return res;
+    }
+};
