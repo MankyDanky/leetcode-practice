d2f7f92e066c4725c20a97be7932e27437a4470d
Aadi Kulshrestha <62843416+MankyDanky@users.noreply.github.com>
2026-07-18 09:45:16 -0700

    Create find_gcd_array.cpp

---
+++ find_gcd_array.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findGCD(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        return gcd(nums[nums.size()-1], nums[0]);
+    }
+
+    int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+};
